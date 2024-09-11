4bd9c98c96eb5637a2ee59f270e63ad3785e5874
igorSimash <vigorochok@gmail.com>
2024-09-11 17:20:09 +0300

    Created all models

---
+++ app/models/tweet.rb
@@ -0,0 +1,10 @@
+class Tweet < ApplicationRecord
+  belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  belongs_to :origin, class_name: "Tweet", optional: true
+  has_many :retweets, class_name: "Tweet", foreign_key: "origin_id"
+
+  validates :body, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :tweets, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
 end
