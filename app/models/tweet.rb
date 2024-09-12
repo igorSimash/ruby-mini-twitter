bc4619b2b67c87646166ebfcf09d0dc3e2507652
igorSimash <vigorochok@gmail.com>
2024-09-12 17:36:20 +0300

    Added length validation

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -6,5 +6,5 @@ class Tweet < ApplicationRecord
   belongs_to :origin, class_name: "Tweet", optional: true
   has_many :retweets, class_name: "Tweet", foreign_key: "origin_id"
 
-  validates :body, presence: true
+  validates :body, presence: true, length: { maximum: 300 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
 end
