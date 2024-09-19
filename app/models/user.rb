5dddc4ef495d5f44980b8d6b9eb41961c59c21f0
igorSimash <vigorochok@gmail.com>
2024-09-19 17:41:07 +0300

    added views

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
 
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
