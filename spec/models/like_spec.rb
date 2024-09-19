bdf4ed9daede251167d00b795186e448ae748031
igorSimash <vigorochok@gmail.com>
2024-09-19 17:59:39 +0300

    transfered subject to like validation conxtext

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -11,17 +11,17 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  let(:user) { User.new(username: "usernameex", email: "user@example.com", password: "password123") }
-  let(:tweet) { Tweet.new(user: user, body: "This is a tweet") }
-
-  subject { Like.new(user: user, tweet: tweet) }
-
   context 'associations' do
     it { is_expected.to belong_to(:user) }
     it { is_expected.to belong_to(:tweet) }
   end
 
   context 'validations' do
+    let(:user) { User.new(username: "usernameex", email: "user@example.com", password: "password123") }
+    let(:tweet) { Tweet.new(user: user, body: "This is a tweet") }
+
+    subject { Like.new(user: user, tweet: tweet) }
+
     it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:tweet_id) }
   end
 end
