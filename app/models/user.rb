cff5c10ced4d4efee67a54cec7403701ebf62656
Ihor Simashko <52800298+igorSimash@users.noreply.github.com>
2024-09-24 10:52:15 +0300

    Closes 05: Devise UI (#11)
    * added views
    
    * added devise edit view; added login by username OR email; new username validation
    
    * fixed tailwind components import
    
    * changed .gitignore
    
    * Delete config/credentials directory
    
    * added inline_svg, fixed user pass err msg, fixed .gitignore
    
    * little fixes
    
    * small class change

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,32 @@
 #
 # Table name: users
 #
-#  id                 :bigint           not null, primary key
-#  username           :string           default(""), not null
-#  email              :string           default(""), not null
-#  encrypted_password :string           default(""), not null
-#  created_at         :datetime         not null
-#  updated_at         :datetime         not null
+#  id                  :bigint           not null, primary key
+#  username            :string           default(""), not null
+#  email               :string           default(""), not null
+#  encrypted_password  :string           default(""), not null
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  remember_created_at :datetime
 #
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :rememberable, :validatable
+  attr_accessor :login
+
+  devise :database_authenticatable, :registerable, :validatable, :rememberable
 
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
 
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 20 }, format: { with: /\A[a-zA-Z0-9_]+\z/, message: "can only contain Latin letters, numbers, and underscores" }
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
+
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(
+      [ "lower(username) = :value OR lower(email) = :value",
+      { value: login.strip.downcase } ]).first
+  end
 end
