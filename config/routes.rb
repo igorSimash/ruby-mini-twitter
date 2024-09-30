222f0ca1d3cb8642f238c7cf9f7fa2fb9d6f3b1f
igorSimash <vigorochok@gmail.com>
2024-09-30 16:38:58 +0300

    fixed diff root_path logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  root "home#index"
+  devise_scope :user do
+    authenticated :user do
+      root "devise/registrations#edit", as: :authenticated_root # TODO: Change after creating tweets#index
+    end
+  end
+
+  unauthenticated :user do
+    root "home#index", as: :unauthenticated_root
+  end
+
   devise_for :users, only: [ :sessions, :registrations ]
 
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
