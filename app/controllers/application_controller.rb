4b82f83b3f9e942fd73b6090696153b02426c52e
igorSimash <vigorochok@gmail.com>
2024-10-02 19:20:48 +0300

    deleted protect_from_forgery with: :null_session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,7 @@
 class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
-  # protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
