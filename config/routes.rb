698385cb16de53279eeac8d2bc98c3ed7f35f378
Ihor Simashko <52800298+igorSimash@users.noreply.github.com>
2024-10-03 12:22:54 +0300

    Basic layout (#14)
    * some refactoring
    
    * added header, home page, some styles
    
    * added new tailwind component + refactoring
    
    * tailwind components refactoring
    
    * Update app/controllers/home_controller.rb
    
    Co-authored-by: Ivan Marynych <49816584+loqimean@users.noreply.github.com>
    
    * resolved changes
    
    * fixed diff root_path logic
    
    * added error message for login
    
    * Update development.rb
    
    * refactor
    
    ---------
    
    Co-authored-by: Ivan Marynych <49816584+loqimean@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    authenticated :user do
+      root "devise/registrations#edit", as: :authenticated_root # TODO: Change after creating tweets#index
+    end
+  end
+
+  unauthenticated :user do
+    root "home#index", as: :unauthenticated_root
+  end
+
   devise_for :users, only: [ :sessions, :registrations ]
-  get "up" => "rails/health#show", as: :rails_health_check
 
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # root "posts#index"
 end
