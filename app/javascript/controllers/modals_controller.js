088b5bca51525972a5ba8f09589c445c62ceefe0
igorSimash <vigorochok@gmail.com>
2024-10-09 16:56:02 +0300

    fixed modal close on form submit logic

--- app/javascript/controllers/modals_controller.js
+++ app/javascript/controllers/modals_controller.js
@@ -2,8 +2,6 @@ import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   close(e) {
-    if (e.type === "turbo:submit-end" && !e.detail.success) return;
-
     const modal = document.getElementById("modal");
     modal.innerHTML = "";
     modal.removeAttribute("src");
